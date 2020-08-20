051c71fe6c22ff387833c1b6b64e4da3fd942c29
19042965 <19042965@LAPTOP-G59321GG>
2020-08-20 22:12:42 +0800

    REQUEST QUOTE class is created

--- C206_CaseStudy/src/requestquote.java
+++ C206_CaseStudy/src/requestquote.java
@@ -1,4 +1,55 @@
 
 public class requestquote {
+	private String propertytype;
+	private int areasize;
+	private String requestname;
+	private int contactnumber;
+	private int budget;
+	private String renovationtype;
+	private int roomsrenovate;
+	private int toiletren;
+	private boolean urgent;
+	public requestquote(String propertytype, int areasize, String requestname, int contactnumber, int budget,
+			String renovationtype, int roomsrenovate, int toiletren, boolean urgent) {
+	
+		this.propertytype = propertytype;
+		this.areasize = areasize;
+		this.requestname = requestname;
+		this.contactnumber = contactnumber;
+		this.budget = budget;
+		this.renovationtype = renovationtype;
+		this.roomsrenovate = roomsrenovate;
+		this.toiletren = toiletren;
+		this.urgent = urgent;
+	}
+	public String getPropertytype() {
+		return propertytype;
+	}
+	public int getAreasize() {
+		return areasize;
+	}
+	public String getRequestname() {
+		return requestname;
+	}
+	public int getContactnumber() {
+		return contactnumber;
+	}
+	public int getBudget() {
+		return budget;
+	}
+	public String getRenovationtype() {
+		return renovationtype;
+	}
+	public int getRoomsrenovate() {
+		return roomsrenovate;
+	}
+	public int getToiletren() {
+		return toiletren;
+	}
+	public boolean isUrgent() {
+		return urgent;
+	}
+	
+	
 
 }
